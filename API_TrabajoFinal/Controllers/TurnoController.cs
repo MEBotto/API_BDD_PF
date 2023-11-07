ca735c1593384e78f87d347768d2f7ddac1e310f
Mariano Botto <marianobotto@gmail.com>
2023-11-07 18:40:37 -0300

    En TurnoController cuando la direccion es null devuelve "Sin dirección"

--- API_TrabajoFinal/Controllers/TurnoController.cs
+++ API_TrabajoFinal/Controllers/TurnoController.cs
@@ -30,6 +30,17 @@ namespace API_TrabajoFinal.Controllers
                 //listaTurnosInfo = _dbContext.Materias.Include(t => t.NroLegajoPNavigation.CodTituloNavigation).Include(t => t.Planificacions).ToList();
                 listaTurnosInfo2 = _dbContext.Turnos.Include(t => t.Planificacions).ThenInclude(p => p.CodMatNavigation).ThenInclude(p => p.NroLegajoPNavigation).ToList();
 
+                foreach (var turno in listaTurnosInfo2)
+                {
+                    foreach (var planificacion in turno.Planificacions)
+                    {
+                        if (planificacion.CodMatNavigation.NroLegajoPNavigation.Direccion == null)
+                        {
+                            planificacion.CodMatNavigation.NroLegajoPNavigation.Direccion = "Sin dirección";
+                        }
+                    }
+                }
+
                 return StatusCode(StatusCodes.Status200OK, new { message = "ok", response = listaTurnosInfo2 });
             }
             catch (Exception ex)
@@ -37,5 +48,24 @@ namespace API_TrabajoFinal.Controllers
                 return StatusCode(StatusCodes.Status200OK, new { message = ex.Message, response = new { } });
             }
         }
+
+        /*[HttpGet]
+        [Route("inffo")]
+        public IActionResult alumnosIncriptosPorTurno()
+        {
+            //List<Materia> listaTurnosInfo = new List<Materia>();
+            List<Turno> listaTurnosInfo2 = new List<Turno>();
+            try
+            {
+                //listaTurnosInfo = _dbContext.Materias.Include(t => t.NroLegajoPNavigation.CodTituloNavigation).Include(t => t.Planificacions).ToList();
+                listaTurnosInfo2 = _dbContext.Turnos.Include(t => t.Planificacions).ThenInclude(p => p.CodMatNavigation).ThenInclude(p => p.NroLegajoPNavigation).ToList();
+
+                return StatusCode(StatusCodes.Status200OK, new { message = "ok", response = listaTurnosInfo2 });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message, response = new { } });
+            }
+        }*/
     }
 }
